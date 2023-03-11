e5d785768ceddd57a209b637d7e72999812785ac
Даниил <derendervad@gmail.com>
2023-03-11 12:25:31 +0300

    change

--- HomeworkPP 5.3.cpp
+++ HomeworkPP 5.3.cpp
@@ -1,43 +1,33 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
 
 class functor {
-    int* arr;
-    int size;
     int sum;
     int count;
 public:
-    functor(int arr[], const int size) : sum{0}, count{0} {
-        this->arr = arr;
-        this->size = size;
+    functor() : sum{0}, count{0} { }
+    void operator()(int x) {
+        if (x % 3 == 0) {
+            sum += x; ++count;
+        }
     }
     void get_sum() {
-        for (int i = 0; i < size; ++i) {
-            if (arr[i] % 3 == 0) {
-                sum += arr[i];
-            }
-        }
         std::cout << "[OUT]: get_sum() = " << sum << std::endl;
     }
     void get_count() {
-        for (int i = 0; i < size; ++i) {
-            if (arr[i] % 3 == 0) {
-                ++count;
-            }
-        }
         std::cout << "[OUT]: get_count() = " << count << std::endl;
     }
-    ~functor() {
-
-    }
+    ~functor() { }
 };
 
 int main()
 {
-    const int size = 6;
-    int arr[size] = { 4, 1, 3, 6, 25, 54 };
-    functor f(arr, size);
+    functor f;
+    std::vector<int> vec = { 4, 1, 3, 6, 25, 54 };
+    f = std::for_each(vec.begin(), vec.end(), f);
     std::cout << "[IN]:";
-    for (int x : arr) {
+    for (int x : vec) {
         std::cout << " " << x;
     } std::cout << std::endl;
     f.get_sum();
